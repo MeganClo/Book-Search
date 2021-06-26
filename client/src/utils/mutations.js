707905866b6e0fa93fc9a11232a85fc055ea32a2
MeganClose <megs.close@gmail.com>
2021-06-26 14:59:52 -0700

    mutations-check

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,63 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookInfo: bookInfo!) {
+        saveBook(bookInfo: $bookInfo) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                description
+                authors
+                image
+                link
+                title
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookInfo: bookInfo!) {
+        removeBook(bookInfo: $bookInfo) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                description
+                authors
+                image
+                link
+                title
+            }
+        }
+    }
+`;
